4337104a69d640f7b494768a8f00cd56f3864636
BugTeaON <rinkee365@outlook.com>
2025-01-19 15:45:12 +0800

    拆分Windows版启动脚本的合并项检测功能到合并脚本中，并改良启动参数

--- auto_merge_all.bat
+++ auto_merge_all.bat
@@ -1,77 +1,109 @@
 @echo off
-
-chcp 65001
 set PCUBErr=0
-set PCUBLD="%cd%"
-cd /d "%~dp0"
-
-if "%1" neq "nocheck" echo.
-if "%1" equ "" title 盘灵无界配置文件一键自动合并
-
-:: jq程序来自：https://github.com/jqlang/jq
-:: yq程序来自：https://github.com/mikefarah/yq
-
-:: 服务端部署包中默认为i386版本（支持X86/64），如果是特殊平台请自行替换文件
-set PCUBJQPATH=.\tools\jq-windows-i386.exe
-set PCUBYQPATH=.\tools\yq_windows_386.exe
-
-echo | set /p=使用部署包自带的 jq: 
-%PCUBJQPATH% --version || goto allerr
-
-echo | set /p=使用部署包自带的 yq: 
-%PCUBYQPATH% --version || goto allerr
+if "%1" neq "check" call :begin %1
+if %PCUBErr% equ 1 goto :eof
 
 :: 遍历所有文件夹，生成或追加列表
-if "%1" neq "nocheck" (
-	echo.
-	echo 正在查找是否有新的合并项...（可使用“nocheck”参数跳过）
 
-	:: 检测目录
-	if exist "*_merge" for /f "delims=" %%i in ('dir /b "*_merge"') do if exist "%%i\" (
-		set PCUBDir=%%i
-		call :chkdir
-	)
+if "%1" neq "check" (
+	echo.
+	echo 正在查找是否有新的合并项...
+)
+set PCUBAllAdded=0
+set PCUBDirAdded=2
+
+:: 检测目录
+if exist "*_merge" for /f "delims=" %%i in ('dir /b "*_merge"') do if exist "%%i\" (
+	set PCUBDir=%%i
+	call :chkdir
 )
 
-:: 遍历列表按顺序合并
-set /p="" < nul > .auto_merge_list_temp
-if exist auto_merge_list.txt for /f "delims=" %%i in (auto_merge_list.txt) do if exist "%%i\" (
-	echo.>>.auto_merge_list_temp
-	set /p="%%i" < nul >> .auto_merge_list_temp
-	set PCUBIndex=%%i
-	call :work
-) else if %PCUBErr% neq 1 (
-	echo.
-	echo 找不到合并项 "%%i"，它将被从列表上移除。这并不影响其他合并项的处理。
-)
-if %PCUBErr% equ 1 goto allerr
+if "%1" neq "check" goto work
 
-:: 保存删减
-move .auto_merge_list_temp auto_merge_list.txt > nul
-:: 生成修改参照文件
-if exist .auto_merge_list_check attrib -h .auto_merge_list_check
-type auto_merge_list.txt > .auto_merge_list_check
-attrib +h .auto_merge_list_check
-:: 组件更新标识
-if exist need_remerge del need_remerge
+:: 对比修改参照文件
+if %PCUBAllAdded% equ 0 if exist auto_merge_list.txt fc auto_merge_list.txt .auto_merge_list_check > nul 2> nul || (
+	echo 检测到合并列表变更。
+	set PCUBAllAdded=1
+)
+
+:: 检测套件更新
+if %PCUBDirAdded% neq 2 if exist need_remerge (
+	echo 检测到被更新的套件。
+	set PCUBAllAdded=1
+)
+
+if %PCUBAllAdded% neq 1 (
+	echo 暂不需要自动合并。
+	goto :eof
+)
 
 echo.
-echo 操作成功完成。
-goto end
+call :loadExec %1
+if %PCUBErr% equ 1 goto :eof
 
+:work
+	:: 遍历列表按顺序合并
+	set /p="" < nul > .auto_merge_list_temp
+	if exist auto_merge_list.txt for /f "delims=" %%i in (auto_merge_list.txt) do if exist "%%i\" (
+		echo.>>.auto_merge_list_temp
+		set /p="%%i" < nul >> .auto_merge_list_temp
+		set PCUBIndex=%%i
+		call :workMerge
+	) else if %PCUBErr% neq 1 (
+		echo.
+		echo 找不到合并项 "%%i"，它将被从列表上移除。这并不影响其他合并项的处理。
+	)
+	if %PCUBErr% equ 1 goto allerr
 
+	:: 保存删减
+	move .auto_merge_list_temp auto_merge_list.txt > nul
+	:: 生成修改参照文件
+	if exist .auto_merge_list_check attrib -h .auto_merge_list_check
+	type auto_merge_list.txt > .auto_merge_list_check
+	attrib +h .auto_merge_list_check
+	:: 套件更新标识
+	if exist need_remerge del need_remerge
+
+	echo.
+	echo 操作成功完成。
+
+	if "%1" equ "check" goto :eof
+	goto end
+:begin
+	chcp 65001
+	set PCUBLD="%cd%"
+	cd /d "%~dp0"
+
+	title 配置文件一键自动合并
+	echo.
+:loadExec
+	:: jq程序来自：https://github.com/jqlang/jq
+	:: yq程序来自：https://github.com/mikefarah/yq
+
+	:: 服务端部署包中默认为i386版本（支持X86/64），如果是特殊平台请自行替换文件
+	set PCUBJQPATH=.\tools\jq-windows-i386.exe
+	set PCUBYQPATH=.\tools\yq_windows_386.exe
+
+	echo | set /p=使用部署包自带的 jq: 
+	%PCUBJQPATH% --version || goto allerr
+
+	echo | set /p=使用部署包自带的 yq: 
+	%PCUBYQPATH% --version || goto allerr
+
+	goto :eof
 :chkdir
 	:: 新增合并项
 	set PCUBDirAdded=0
 	if exist auto_merge_list.txt for /f "delims=" %%j in (auto_merge_list.txt) do if "%PCUBDir%" equ "%%j" set PCUBDirAdded=1
 	if %PCUBDirAdded% equ 0 (
+		set PCUBAllAdded=1
 		echo 发现新的合并项: "%PCUBDir%"
 		echo.>>auto_merge_list.txt
 		set /p="%PCUBDir%" < nul >> auto_merge_list.txt
 	)
 
 	goto :eof
-:work
+:workMerge
 	if %PCUBErr% equ 1 goto :eof
 	echo.
 	echo 正在自动合并 "%PCUBIndex%" 中的配置文件...
@@ -119,6 +151,7 @@ goto end
 	set PCUBErr=1
 	echo.
 	echo 程序执行出错。 
+	if "%1" equ "check" goto :eof
 :end
 	cd /d %PCUBLD%
 	if "%1" equ "" pause
--- start.bat
+++ start.bat
@@ -2,9 +2,7 @@
 :launch
 ::	========================================
 ::	这是启动服务器的关键命令
-	%JavaExec% -Djdk.util.jar.enableMultiRelease=force -Xms2G -Xmx2G -jar .\spigot-1.20.1.jar
-::	参数 -Djdk.util.jar.enableMultiRelease=force 用于在未安装 Floodgate 插件的情况下使基岩版正常打开菜单书 
-::	其他参数和常规服务端通用，如需自行修改或制作脚本，则只需要注意上述参数即可
+	%JavaExec% -Xms1G -Xmx2G -Dfile.encoding=utf-8 -DGeyser.ShowResourcePackLengthWarning=false -jar .\leaves-1.20.1.jar nogui
 ::	========================================
 
 	set PCUBErr=%errorLevel%
@@ -19,19 +17,23 @@
 	goto end
 :begin
 	chcp 65001
-	if "%1" equ "" title 盘灵无界·互通服务端
+	if "%1" equ "" title 梦回盘灵互通服务端
 	set PCUBLDA="%cd%"
 	cd /d "%~dp0"
 
 	if not exist "plugins\Geyser-Spigot\custom_mappings\pcub.json" (
 		echo.
-		echo 检测到您未安装盘灵无界基础必要组件，无法启动服务器！
+		echo 检测到您未安装梦回盘灵 Java - 基岩双端互通套件，无法启动服务器！
 		goto end
 	)
 
 	:: 遍历所有文件夹，生成或追加列表
-	set PCUBErr=0
-	if "%1" neq "nocheck" call :chkall
+	set PCUBErr=1
+	if "%1" neq "nocheck" (
+		echo.
+		echo 正在检测合并项...（可使用“nocheck”参数跳过）
+		call auto_merge_all.bat check
+	)
 	if %PCUBErr% equ 1 goto err
 
 	echo.
@@ -39,51 +41,6 @@
 	if "%JavaExec%" equ "" set JavaExec=java.exe
 
 	goto launch
-:chkall
-	echo.
-	echo 正在检测合并项...（可使用“nocheck”参数跳过）
-
-	set PCUBAllAdded=0
-	set PCUBDirAdded=2
-
-	:: 检测目录
-	if exist "*_merge" for /f "delims=" %%i in ('dir /b "*_merge"') do if exist "%%i\" (
-		set PCUBDir=%%i
-		call :chkdir
-	)
-
-	:: 对比修改参照文件
-	if exist auto_merge_list.txt fc auto_merge_list.txt .auto_merge_list_check > nul 2> nul || if %PCUBAllAdded% equ 0 (
-		echo 检测到合并列表变更。
-		set PCUBAllAdded=1
-	)
-
-	:: 检测组件更新
-	if %PCUBDirAdded% neq 2 if exist need_remerge (
-		echo 检测到组件被更新。
-		set PCUBAllAdded=1
-	)
-
-	if %PCUBAllAdded% equ 1 (
-		echo.
-		echo 正在执行自动合并...
-		call auto_merge_all.bat nocheck
-	) else (
-		echo 暂不需要自动合并。
-	)
-	goto :eof
-:chkdir
-	:: 新增合并项
-	set PCUBDirAdded=0
-	if exist auto_merge_list.txt for /f "delims=" %%j in (auto_merge_list.txt) do if "%PCUBDir%" equ "%%j" set PCUBDirAdded=1
-	if %PCUBDirAdded% equ 0 (
-		set PCUBAllAdded=1
-		echo 发现新的合并项: "%PCUBDir%"
-		echo.>>auto_merge_list.txt
-		set /p="%PCUBDir%" < nul >> auto_merge_list.txt
-	)
-
-	goto :eof
 :err
 	echo 若想跳过自动合并检测直接启动服务端，可以使用“nocheck”参数。 
 :end
