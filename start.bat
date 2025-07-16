2cacc98cb624e2edc397b54103417ffe7e7f11f2
BugTeaON <rinkee365@outlook.com>
2025-07-16 16:01:42 +0800

    启动前检测地图和梦盘数据包；新增两个参数用于单独跳过自动合并和文件环境检测

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off && goto begin
 :launch
 ::	========================================
-::	这是启动服务器的关键命令
+::	这是启动服务端的关键命令
 	%JavaExec% -Xms1G -Xmx2G -Dfile.encoding=utf-8 -DGeyser.ShowResourcePackLengthWarning=false -jar .\leaves-1.20.1.jar nogui
 ::	========================================
 
@@ -10,40 +10,57 @@
 	if %PCUBErr% equ 9009 (
 		echo 系统找不到 Java ^(%JavaExec%^)。请检查其是否在环境变量中，或使用 JavaExec 变量指定 Java 程序路径。
 	) else if %PCUBErr% gtr 0 (
-		echo 服务器非正常退出。错误码为：%PCUBErr%
+		echo 服务端非正常关闭。错误码为：%PCUBErr%
 	) else (
-		echo 服务器已经安全退出。
+		echo 服务端已经安全关闭。
 	)
 	goto end
 :begin
 	chcp 65001
-	if "%1" equ "" title 梦回盘灵互通服务端
+	if "%1" equ "" title 盘灵古域互通（梦回盘灵）专用服务端
 	set PCUBLDA="%cd%"
 	cd /d "%~dp0"
 
-	if not exist "plugins\Geyser-Spigot\custom_mappings\pcub.json" (
-		echo.
-		echo 检测到您未安装梦回盘灵 Java - 基岩双端互通套件，无法启动服务器！
-		goto end
+	:: 文件环境检测
+	if "%1" equ "nocheck" ( rem 0
+	) else if "%1" equ "noenvcheck" ( rem 0
+	) else if "%2" equ "noenvcheck" ( rem 0
+	) else if not exist "world\data\Temple.dat" (
+		set PCUBEnvErr=未导入盘灵古域地图
+		goto envCheckErr
+	) else if not exist "world\datapacks\panling*" (
+		set PCUBEnvErr=未导入梦回盘灵数据包
+		goto envCheckErr
+	) else if not exist "plugins\Geyser-Spigot\custom_mappings\pcub.json" (
+		set PCUBEnvErr=未合并梦回盘灵专用 Java - 基岩双端互通套件
+		goto envCheckErr
 	)
 
 	:: 遍历所有文件夹，生成或追加列表
 	set PCUBErr=0
-	if "%1" neq "nocheck" (
+	if "%1" equ "nocheck" ( rem 0
+	) else if "%1" equ "nomergecheck" ( rem 0
+	) else if "%2" equ "nomergecheck" ( rem 0
+	) else (
 		set PCUBErr=1
 		echo.
-		echo 正在检测合并项...（可使用“nocheck”参数跳过）
+		echo 正在检测合并项...
 		call auto_merge_all.bat check
 	)
-	if %PCUBErr% equ 1 goto err
+	if %PCUBErr% equ 1 goto mergeErr
 
 	echo.
 	echo 正在启动服务端...
 	if "%JavaExec%" equ "" set JavaExec=java.exe
 
 	goto launch
-:err
-	echo 若想跳过自动合并检测直接启动服务端，可以使用“nocheck”参数。 
+:envCheckErr
+	echo.
+	echo 检测到您%PCUBEnvErr%，无法启动服务端
+	echo 若想跳过环境检测直接启动服务端，可以使用“noenvheck”参数。
+	goto end
+:mergeErr
+	echo 若想跳过自动合并检测直接启动服务端，可以使用“nomergecheck”参数。 
 :end
 	cd /d %PCUBLDA%
 	if "%1" equ "" pause
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function launch(){
 #	========================================
-#	这是启动服务器的关键命令
+#	这是启动服务端的关键命令
 	$JavaExec -Xms1G -Xmx2G -Dfile.encoding=utf-8 -DGeyser.ShowResourcePackLengthWarning=false -jar ./leaves-1.20.1.jar nogui
 #	========================================
 
@@ -10,25 +10,41 @@ function launch(){
 	if [ $PCUBErr = 127 ]; then
 		echo "系统找不到 Java ($JavaExec)。请检查其是否在环境变量中，或使用 JavaExec 变量指定 Java 程序路径。(注意区分大小写)"
 	elif [ $PCUBErr != 0 ]; then
-		echo "服务器非正常退出。错误码为：$PCUBErr"
+		echo "服务端非正常关闭。错误码为：$PCUBErr"
 	else
-		echo "服务器已经安全退出。"
+		echo "服务端已经安全关闭。"
 	fi
 	exit $PCUBErr
 }
 
+function envCheckErr {
+	echo -e "\n检测到您$1，无法启动服务端"
+	echo "若想跳过环境检测直接启动服务端，可以使用“noenvheck”参数。"
+	exit 1
+}
+
 cd "$(cd "$(dirname "$0")";pwd)"
 
-if [ ! -f "plugins/Geyser-Spigot/custom_mappings/pcub.json" ]; then
-	echo -e "\n检测到您未安装梦回盘灵 Java - 基岩双端互通套件，无法启动服务器！"
-	exit 1
+# 文件环境检测
+if [ "$1" = "nocheck" ] ||
+   [ "$1" = "noenvcheck" ] ||
+   [ "$2" = "noenvcheck" ]; then :
+elif [ ! -f "world/data/Temple.dat" ]; then
+	envCheckErr "未导入盘灵古域地图"
+elif [ ! -e world/datapacks/panling* ]; then
+	envCheckErr "未导入梦回盘灵数据包"
+elif [ ! -f "plugins/Geyser-Spigot/custom_mappings/pcub.json" ]; then
+	envCheckErr "未合并梦回盘灵专用 Java - 基岩双端互通套件"
 fi
 
 # 遍历所有文件夹，生成或追加列表
-if [ "$1" != "nocheck" ]; then
-	echo -e "\n正在检测合并项...（可使用“nocheck”参数跳过）"
+if [ "$1" = "nocheck" ] ||
+   [ "$1" = "nomergecheck" ] ||
+   [ "$2" = "nomergecheck" ]; then :
+else
+	echo -e "\n正在检测合并项..."
 	bash auto_merge_all.sh check || {
-		echo "若想跳过自动合并检测直接启动服务端，可以使用“nocheck”参数。"
+		echo "若想跳过自动合并检测直接启动服务端，可以使用“nomergecheck”参数。"
 		exit 1
 	}
 fi
